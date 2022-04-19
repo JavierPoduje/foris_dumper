f6552350660af037c525aa5db7a4ab4fa9a6d30b
JavierPoduje <javier.poduje@gmail.com>
2022-04-18 21:05:54 -0400

    Read client command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-use serde_json::{Value, from_reader};
+use serde_json::{from_reader, Value as JsonValue};
 use std::fs::File;
+use clap::{Arg, Command};
 
 const FILE_NAME: &str = "hosts.json";
 
-fn main() {
+fn hosts_file() -> JsonValue {
     let file = File::open(FILE_NAME).expect("file should open read only");
-    let json: Value = from_reader(file).expect("file should be proper JSON");
-    println!("{}", json);
+    let json: JsonValue = from_reader(file).expect("file should be proper JSON");
+    json
+}
+
+fn main() {
+    let args = Command::new("Tags Handler")
+        .version("0.1")
+        .about("Bring tags from Foris clients")
+        .arg(
+            Arg::new("client")
+                .long("client")
+                .help("name of the client (example: 'ueuropea-qa')")
+                .takes_value(true)
+                .required(true)
+        )
+        .get_matches();
+
+    let client = args.value_of("client").unwrap();
+    let file = hosts_file();
+
+    let hosts = file.get("hosts").unwrap();
+    let info = hosts.get(client);
+    println!("{:?}", info);
 }
