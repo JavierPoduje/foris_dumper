236c7beddb904c5092b4b029ba89947365b2c3e6
JavierPoduje <javier.poduje@gmail.com>
2022-04-19 21:08:28 -0400

    Get dump from remote environment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use clap::{Arg, Command};
 use serde_json::{from_reader, Value as JsonValue};
 use std::fs::File;
-use clap::{Arg, Command};
+use std::process::{Command as PCommand, Output};
+
+use dotenv;
 
 const FILE_NAME: &str = "hosts.json";
 
@@ -10,7 +13,23 @@ fn hosts_file() -> JsonValue {
     json
 }
 
-fn main() {
+fn dump(client_info: &JsonValue) -> Output {
+    let ssh_alias = dotenv::var("SSH_ALIAS").unwrap();
+    let host = client_info.get("host").unwrap();
+    let username = client_info.get("username").unwrap();
+    let password = client_info.get("password").unwrap();
+    let scenarios_db = client_info.get("scenarios_db").unwrap();
+
+    PCommand::new("ssh")
+        .args([
+            &ssh_alias,
+            &format!("mysqldump -e --host={} --user={} --password={} --port=3306 --max_allowed_packet=1024M {} tags model_extensions", host, username, password, scenarios_db),
+        ])
+        .output()
+        .expect("Couldn't get the dump, dude...")
+}
+
+fn main() -> Result<(), ()> {
     let args = Command::new("Tags Handler")
         .version("0.1")
         .about("Bring tags from Foris clients")
@@ -19,7 +38,7 @@ fn main() {
                 .long("client")
                 .help("name of the client (example: 'ueuropea-qa')")
                 .takes_value(true)
-                .required(true)
+                .required(true),
         )
         .get_matches();
 
@@ -27,6 +46,10 @@ fn main() {
     let file = hosts_file();
 
     let hosts = file.get("hosts").unwrap();
-    let info = hosts.get(client);
-    println!("{:?}", info);
+    let client_info = hosts.get(client).unwrap();
+
+    let output = dump(client_info);
+    println!("{:?}", output);
+
+    Ok(())
 }
